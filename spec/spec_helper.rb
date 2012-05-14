0b5ced5b5bda2591a66ecb9bf1174c473a556c12
Daniel Demmel <daniel.demmel@gmail.com>
2012-05-14 23:42:18 +0100

    Setting environment up for tests.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,17 @@
+require 'rspec'
 require 'guard/steering'
-ENV["GUARD_ENV"] = 'test'
+
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+  
+  config.before(:each) do
+    ENV["GUARD_ENV"] = 'test'
+    @project_path    = Pathname.new(File.expand_path('../../', __FILE__))
+  end
+
+  config.after(:each) do
+    ENV["GUARD_ENV"] = nil
+  end
+end
