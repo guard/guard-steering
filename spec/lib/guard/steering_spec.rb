413a2b7952b7132058d423be494e032b30e6d11c
Daniel Demmel <daniel.demmel@gmail.com>
2012-05-16 17:27:07 +0100

    Starting to stub tests out.

--- spec/lib/guard/steering_spec.rb
+++ spec/lib/guard/steering_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
+require 'fakefs/spec_helpers'
 
 describe "Guard-Steering" do
+  include FakeFS::SpecHelpers
+  
+  let(:guard) { Guard::Steering.new }
+  let(:watcher) { Guard::Watcher.new(%r{^yes/.+\.handlebars$}) }
+  
+  let(:klass) { Guard::Steering }
+  it "inherits from Guard" do
+    klass.ancestors.should include Guard::Guard
+  end
+  
   
 end
