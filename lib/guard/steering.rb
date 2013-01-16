7b5a8156fd4018c9e258d3eb4df08fa24f875612
Daniel Demmel <dain@danieldemmel.me>
2013-01-16 15:32:52 +0000

    Adding :quiet option to silence command line output.

--- lib/guard/steering.rb
+++ lib/guard/steering.rb
@@ -14,7 +14,8 @@ module Guard
       @options = {
         :run_at_start => true,
         :output_folder => nil,
-        :register_partials => false
+        :register_partials => false,
+        :quiet => false
       }.update(options)
 
       @output_folder = !@options[:output_folder].nil? && @options[:output_folder]
@@ -25,7 +26,7 @@ module Guard
     # Call once when Guard starts. Please override initialize method to init stuff.
     # @raise [:task_has_failed] when start has failed
     def start
-      UI.info("Guard::Steering has started watching your files with output folder set to '#{@output_folder}' (in case of 'nil' templates will be compiled to the folder where they are)")
+      UI.info("Guard::Steering has started watching your files with output folder set to '#{@output_folder}' (in case of 'nil' templates will be compiled to the folder where they are)") unless @options[:quiet]
 
       run_all if @options[:run_at_start]
     end
@@ -67,16 +68,16 @@ module Guard
       paths.each do |path|
         output_folder = @output_folder || File.dirname(path)
         File.exists?(output_folder + "/" + File.basename(path) + ".js")
-        UI.info "Steering deleted #{File.basename(path)}.js from #{output_folder}"
+        UI.info "Guard::Steering deleted #{File.basename(path)}.js from #{output_folder}" unless @options[:quiet]
       end
     end
     
     def run_steering(path, output_folder)
       begin
         ::Steering.compile_to_file(path, output_folder + "/" + File.basename(path) + ".js", {:extension => '.handlebars', :partial => @options[:register_partials]})
-        UI.info "Steering precompiled #{path} to #{output_folder}"
+        UI.info "Guard::Steering precompiled #{path} to #{output_folder}" unless @options[:quiet]
       rescue Exception => e
-        UI.error "Steering precompilation failed: #{e}"
+        UI.error "Guard::Steering precompilation failed: #{e}" unless @options[:quiet]
         false
       end
     end
